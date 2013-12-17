0e19a15d9422cb3f9309b4adb127f96c6f0b6e9d
ninad chilap <ninadchilap@gmail.com>
2013-12-17 15:15:34 +0530

    Subject: Updated base files for Navbar and anchor color
    Description:
    
    1) Changed the color of menu items which where inactive to grey.
    2) Highlighted the active menu item(base.js is added for this)
    3) Changed color of on hover.
    4) Interchanged the row of content of proxyMITY and Clicker in base.html.
    5) Changed anchor link color from green to blue.

---
+++ static/js/base.js
@@ -0,0 +1,32 @@
+/* Getting the current url*/
+ var url = window.location.href;
+
+/* cropping the unwanted address from the url */
+
+var urls = url.split('/');
+
+var str = "/";
+
+for(i=3;i<urls.length-1;i++)
+{
+	str += urls[i] + "/";
+}
+
+var len = $('.navbar-nav li').length;
+
+
+for(i=0;i<len;i++)
+{
+var href = $(".navbar-nav li:eq("+(i)+") a").attr('href'); 
+
+	if(href == str)
+	{
+		$(".navbar-nav li:eq("+(i)+")").addClass("active");
+	}
+	else{
+		$(".navbar-nav li:eq("+(i)+")").removeClass('active');
+	}
+
+}
+
+
