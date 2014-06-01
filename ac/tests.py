76f37df387b305e012fd3f29b51be2f4f97c231e
psachin <iclcoolster@gmail.com>
2014-06-01 23:06:53 +0530

    Subject: UT: on ac.views.index
    Description:
    
    - startswith('<html>')
    - contains(TITLE)
    - find('</html>')#FIXME: should be endswith()

--- ac/tests.py
+++ ac/tests.py
@@ -6,11 +6,24 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
+from django.http import HttpRequest
 
+from ac.views import index
 
 class SimpleTest(TestCase):
     def test_basic_addition(self):
         """
         Tests that 1 + 1 always equals 2.
         """
-        self.assertEqual(1 + 1, 2)
+        self.assertEqual(3 * 5, 15)
+
+
+class HomePageTest(TestCase):
+    """Test aakashlabs.org home page"""
+    def test_index(self):
+        request = HttpRequest()
+        response = index(request)
+        self.assertTrue(response.content.startswith('<!DOCTYPE html>'))
+        self.assertIn('<title>Aakashlabs</title>', response.content)
+        #FIXME:Should be endswith
+        self.assertTrue(response.content.find('</html>'))
