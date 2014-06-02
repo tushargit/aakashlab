58188944d5618608b080257434d65eb8a031bb0f
psachin <iclcoolster@gmail.com>
2014-06-02 18:08:47 +0530

    Subject: UT: Make sure correct index html page is loaded.
    Description:
    
    - Check for model 'Coordinator' failed, coordinator should be a user instance.

--- ac/tests.py
+++ ac/tests.py
@@ -8,8 +8,12 @@ Replace this with more appropriate tests for your application.
 from django.test import TestCase
 from django.http import HttpRequest
 from django.core.urlresolvers import resolve
+from django.template.loader import render_to_string
 
 from ac.views import index, about
+
+from ac.models import Coordinator
+
 
 class SimpleTest(TestCase):
     def test_basic_addition(self):
@@ -21,6 +25,8 @@ class SimpleTest(TestCase):
 
 class HomePageTest(TestCase):
     """Test aakashlabs.org home page"""
+
+    
     def test_index(self):
         request = HttpRequest()
         response = index(request)
@@ -29,7 +35,22 @@ class HomePageTest(TestCase):
         self.assertTrue(response.content.strip().endswith('</html>'))
 
 
+    def test_returns_correct_html(self):
+        request = HttpRequest()
+        response = index(request)        
+        index_html = render_to_string('index.html')
+        self.assertEqual(response.content, index_html)
+
+
     def test_root_url_resolve_to_index(self):
         root_url = resolve('/')
         self.assertEquals(root_url.func, index)
         
+
+
+class AcModelTest(TestCase):
+    """Check models in 'ac' app."""
+
+    def test_coordinator_model(self):
+        coordinator = Coordinator()
+        coordinator.user = "admin"
