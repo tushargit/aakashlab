8f2fb180420dc43b2a5047a71305c18aaaa74eb5
psachin <iclcoolster@gmail.com>
2014-09-04 15:11:26 +0530

    Subject: Added redirect app to redirect pages for special apps.
    Ex:
    
    aakashlabs.org/apl
    aakashlabs.org/abt

--- aakashlabs/settings.py
+++ aakashlabs/settings.py
@@ -114,6 +114,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'django.contrib.redirects.middleware.RedirectFallbackMiddleware',
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
@@ -139,6 +140,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
+    'django.contrib.redirects',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     # South
